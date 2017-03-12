0ca2bd3b1fbe6906869fb02d033b474e35233551
Filip Brcic <brcha@gna.org>
2017-03-12 23:02:37 +0100

    Beautified the code, added haddock and optimized a bit

--- library/ErepApiDb.hs
+++ library/ErepApiDb.hs
@@ -1,31 +1,29 @@
 {-# OPTIONS -XOverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
--- ----- BEGIN LICENSE BLOCK -----
--- Version: MPL 2.0
---
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
---
--- ----- END LICENSE BLOCK -----
 
+-- |
+-- Module      :  ErepApiDb
+-- Copyright   :  (c) 2017, Filip Brcic
+-- License     :  MPL-2.0
+-- Maintainer  :  Filip Brcic <brcha@gna.org>
+-- Stability   :  experimental
+-- Portability :  portable
 --
--- Copyright (c) 2017, Filip Brcic <brcha@gna.org>. All rights reserved.
---
--- This file is part of ErepApiDb
---
+-- The 'main' function of the eRepApiDB program/library
 
--- | An example module.
 module ErepApiDb (
     main
   , module ErepApiDb.ErepublikApi
 ) where
 
-import ErepApiDb.ErepublikApi
+import           ErepApiDb.ErepublikApi
 
-import Control.Monad
-import Env
+import           Control.Monad
+import           Env
 
 data ErepApiDbOptions = ErepApiDbOptions { publicKey :: String, privateKey :: String}
 
@@ -34,7 +32,8 @@ erepApiDbOptions = Env.parse (header "ErepApiDb") $
     ErepApiDbOptions <$> var (str <=< nonempty) "EREP_PUBLIC_KEY"  (help "Public key for eRepublik API")
                      <*> var (str <=< nonempty) "EREP_PRIVATE_KEY" (help "Private key for eRepublik API")
 
--- | An example function.
+-- | The 'main' function of the program
+--   Currently under construction :)
 main :: IO ()
 main = do
   ErepApiDbOptions {publicKey, privateKey} <- erepApiDbOptions
--- library/ErepApiDb/ErepublikApi.hs
+++ library/ErepApiDb/ErepublikApi.hs
@@ -1,57 +1,63 @@
 {-# OPTIONS -XOverloadedStrings #-}
 
--- ----- BEGIN LICENSE BLOCK -----
--- Version: MPL 2.0
---
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
---
--- ----- END LICENSE BLOCK -----
 
+-- |
+-- Module      :  ErepApiDb.ErepublikApi
+-- Copyright   :  (c) 2017, Filip Brcic
+-- License     :  MPL-2.0
+-- Maintainer  :  Filip Brcic <brcha@gna.org>
+-- Stability   :  experimental
+-- Portability :  portable
 --
--- Copyright (c) 2017, Filip Brcic <brcha@gna.org>. All rights reserved.
---
--- This file is part of ErepApiDb
---
+-- The eRepublik 'api_call' function.
 
 module ErepApiDb.ErepublikApi (
-    hash_hmac_sha256
-  , api_call
+    api_call
 ) where
 
-import Crypto.MAC.HMAC
-import Crypto.Hash hiding (Context)
-import qualified Data.ByteString as B
+import           Crypto.Hash           hiding (Context)
+import           Crypto.MAC.HMAC
+import qualified Data.ByteString       as B
 import qualified Data.ByteString.Char8 as BC8
-import qualified Data.ByteString.Lazy as BL
-import Data.Char
+import qualified Data.ByteString.Lazy  as BL
+import           Data.Char
 
-import Web.FormUrlEncoded
+import           Web.FormUrlEncoded
 
-import Network.Curl
+import           Network.Curl
 
-import Data.Time.Clock
-import Data.Time.Format
+import           Data.Time.Clock
+import           Data.Time.Format
 
+import           Data.Monoid
+
+-- | 'hash_hmac_sha256' returns hmac hash using SHA256
 hash_hmac_sha256 :: B.ByteString -> B.ByteString -> HMAC SHA256
 hash_hmac_sha256 = hmac
 
--- Hardcoded private/public keys might be a bad idea
-api_call :: (String, String) -> String -> String -> [(String, Int)] -> IO (CurlCode, String)
+-- | 'api_call' is the main function used to interact with api.erepublik.com server
+api_call :: (String, String)      -- ^ Public and private keys for eRepublik's API
+         -> String                -- ^ Resource that is being accessed
+         -> String                -- ^ Action on the resource
+         -> [(String, Int)]       -- ^ List of parameters for the action (or empty list)
+         -> IO (CurlCode, String) -- ^ Returns Curl's exit code and the returned yaml code
 api_call (public, private) resource action query = do
       currentTime <- getCurrentTime
       let
         timeString = formatTime defaultTimeLocale rfc1123DateFormat currentTime
-        messageString = BC8.concat [ (BC8.map toLower $ BC8.pack $ resource ++ ":" ++ action ++ digestAddon ++ ":"), (BC8.pack timeString) ]
+        resourceUrlPart = mconcat [ resource, ":", action, digestAddon, ":" ]
+        messageString = BC8.concat [ ( BC8.map toLower . BC8.pack $ resourceUrlPart ), (BC8.pack timeString) ]
         digest = hash_hmac_sha256 (BC8.pack private) messageString
-        headers = [("Date: " ++ timeString),
-                   ("Auth: " ++ public ++ "/" ++ (show $ hmacGetDigest digest))]
-        curlOptions = method_GET ++ [ (CurlHttpHeaders headers) ]
+        headers = [("Date: " <> timeString),
+                   ("Auth: " <> public <> "/" <> (show $ hmacGetDigest digest) ) ]
+        curlOptions = method_GET <> [ (CurlHttpHeaders headers) ]
       curlGetString urlString curlOptions
     where
-      urlString = "http://api.erepublik.com/" ++ resource ++ "/" ++ action ++ queryAddon
-      queryAddon = if length query > 0 then "?" ++ urlEncodedQuery else ""
-      digestAddon = if length query > 0 then ":" ++ urlEncodedQuery else ""
-      urlEncodedQuery = BC8.unpack $ BL.toStrict $ urlEncodeAsForm query
+      urlString = "http://api.erepublik.com/" <> resource <> "/" <> action <> queryAddon
+      queryAddon = if not (null query) then "?" <> urlEncodedQuery else ""
+      digestAddon = if not (null query) then ":" <> urlEncodedQuery else ""
+      urlEncodedQuery = BC8.unpack . BL.toStrict $ urlEncodeAsForm query
       rfc1123DateFormat = "%a, %0d %b %Y %H:%M:%S %z"
